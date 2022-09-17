ef7d6945f53e52034241679ed136e77e106cd0ea
UllrS <94692310+UllrS@users.noreply.github.com>
2022-09-17 23:55:05 +0300

    Create Banner.swift

---
+++ Banner.swift
@@ -0,0 +1,64 @@
+// add this to init app
+//init() {
+//    GADMobileAds.sharedInstance().start(completionHandler: nil)
+//}
+
+import SwiftUI
+import GoogleMobileAds
+
+struct BunnerViewCustom: UIViewRepresentable {
+    
+    var unitID: String = "ca-app-pub-3940256099942544/2934735716"
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+    
+    func makeUIView(context: Context) -> GADBannerView {
+        let adView = GADBannerView(adSize: GADAdSizeBanner)
+        adView.adUnitID = unitID
+        adView.rootViewController = UIApplication.shared.getRootViewController()
+        adView.load(GADRequest())
+        return adView
+    }
+    func updateUIView(_ uiView: GADBannerView, context: Context) {
+        
+    }
+    class Coordinator: NSObject, GADBannerViewDelegate {
+        func bannerViewDidReceiveAd(_ bannerView: GADBannerView) {
+          print("bannerViewDidReceiveAd")
+        }
+
+        func bannerView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: Error) {
+          print("bannerView:didFailToReceiveAdWithError: \(error.localizedDescription)")
+        }
+
+        func bannerViewDidRecordImpression(_ bannerView: GADBannerView) {
+          print("bannerViewDidRecordImpression")
+        }
+
+        func bannerViewWillPresentScreen(_ bannerView: GADBannerView) {
+          print("bannerViewWillPresentScreen")
+        }
+
+        func bannerViewWillDismissScreen(_ bannerView: GADBannerView) {
+          print("bannerViewWillDIsmissScreen")
+        }
+
+        func bannerViewDidDismissScreen(_ bannerView: GADBannerView) {
+          print("bannerViewDidDismissScreen")
+        }
+    }
+}
+
+extension UIApplication {
+    func getRootViewController() -> UIViewController{
+        guard let screen = self.connectedScenes.first as? UIWindowScene else {
+            return .init()
+        }
+        guard let root = screen.windows.first?.rootViewController else {
+            return .init()
+        }
+        return root
+    }
+}
